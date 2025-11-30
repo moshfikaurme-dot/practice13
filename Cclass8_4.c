93b41b91d2b5fc73dfcd00e8cf9afd33a3ef620c
Moshfika Najnin <moshfikaurme@gmail.com>
2025-11-30 19:16:04 +0600

    Add modular student marks program with functions for printing marks and calculating total and average

---
+++ Cclass8_4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*
+void greeting(char nam[]){
+    printf("Good morning %s",nam);
+}
+int main(){
+    char name[100]="Moshfika";
+    greeting (name;)
+}
+*/
+int sum(int a, int b){              //perametar
+int summation=a+b;
+//printf("summation:%d",sum);
+return summation;
+}
+
+int main(){
+    int n1,n2;
+    n1=10;
+    n2=20;
+    //sum(n1,n2);
+    int x=summation(n1,n2);
+    printf("summation:%d",x);
+}
